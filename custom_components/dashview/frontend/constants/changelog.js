cec9c567b8311f6b6791cded142795e5d1aba1f2
mholzi <mholzi@gmail.com>
2026-01-31 11:31:53 +0100

    chore: bump version to 1.5.0-beta.15

--- custom_components/dashview/frontend/constants/changelog.js
+++ custom_components/dashview/frontend/constants/changelog.js
@@ -14,7 +14,7 @@
  * Current version of the installed integration
  * UPDATE THIS with each release - must match manifest.json/const.py
  */
-export const CURRENT_VERSION = '1.5.0-beta.14';
+export const CURRENT_VERSION = '1.5.0-beta.15';
 
 /**
  * Changelog entries - newest first
